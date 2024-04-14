e1a960599e643c8cfcce8cac0a35fc9d27b93532
Loïc <loic.boutet.1@ulaval.ca>
2024-04-14 15:23:16 -0400

    third

--- app.py
+++ app.py
@@ -338,6 +338,3 @@ def get_recent_notes(user_id):
 def is_valid_email(email):
     email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     return re.match(email_regex, email) is not None
-
-if __name__ == '__main__':
-    app.run(debug=True)
